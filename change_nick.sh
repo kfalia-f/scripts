91e137e7c0c0da1fcf96e554da1a330d1430dd59
kfalia-f <unstableperson@yandex.ru>
2020-03-27 16:22:03 +0300

    pars date

--- change_nick.sh
+++ change_nick.sh
@@ -9,20 +9,6 @@ fi
 old_name=$2
 new_name=$3
 
-if [ $old_name = $new_name ]
-then
-	new_6="/*   By: $new_name <marvin@42.fr>                    +#+  +:+       +#+        */"
-	new_8="/*   Created: 2018/11/22 23:05:45 by $new_name          #+#    #+#             */"
-	new_9="/*   Updated: 2019/02/09 16:10:43 by $new_name         ###   ########.fr       */"
-else
-	sp=$(( $(expr length $old_name) - $(expr length $new_name) ))
-	tmp="                                         "
-	new_6="/*   By: $new_name <marvin@42.fr>${tmp:0:20 + sp}+#+  +:+       +#+        */"
-	new_8="/*   Created: 2018/11/22 23:05:45 by $new_name${tmp:0:10 + sp}#+#    #+#             */"
-	new_9="/*   Updated: 2019/02/09 16:10:43 by $new_name${tmp:0:9 + sp}###   ########.fr       */"
-fi
-
-echo $sp
 dir="$1/*"
 
 for file in $dir
@@ -32,6 +18,24 @@ do
 		l=$(awk '/\/\*\ \ \ \ \ /' $file | wc -l)
 		if [ $l -gt 3 ]
 		then
+			tmp=$(sed -n '8p' $file)
+			dt1=${tmp:14:10}
+			tm1=${tmp:25:8}
+			tmp=$(sed -n '9p' $file)
+			dt2=${tmp:14:10}
+			tm2=${tmp:25:8}
+			if [ $old_name = $new_name ]
+			then
+				new_6="/*   By: $new_name <marvin@42.fr>                    +#+  +:+       +#+        */"
+				new_8="/*   Created: $dt1 $tm1 by $new_name          #+#    #+#             */"
+				new_9="/*   Updated: $dt2 $tm2 by $new_name         ###   ########.fr       */"
+			else
+				sp=$(( $(expr length $old_name) - $(expr length $new_name) ))
+				tmp="                                         "
+				new_6="/*   By: $new_name <marvin@42.fr>${tmp:0:20 + sp}+#+  +:+       +#+        */"
+				new_8="/*   Created: $dt1 $tm1 by $new_name${tmp:0:10 + sp}#+#    #+#             */"
+				new_9="/*   Updated: $dt2 $tm2 by $new_name${tmp:0:9 + sp}###   ########.fr       */"
+			fi
 			sed -i "6c\\$new_6" $file
 			sed -i "8c\\$new_8" $file
 			sed -i "9c\\$new_9" $file
